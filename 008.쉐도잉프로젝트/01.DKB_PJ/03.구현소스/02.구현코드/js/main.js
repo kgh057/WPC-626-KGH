9754753c4b53ee6e8b51d24eab8458856f18b92b
KGH <ehsrhdiddl@naver.com>
2025-10-02 13:02:17 +0900

    Update main.js

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
@@ -109,3 +109,16 @@ videoSwiper.on("slideChange", () => {
     btnPrev.style.display = "block";
   } /// else ////
 }); ///// slideChange //////
+
+/************************************************ 
+    햄버거 버튼 클릭시 상단영역에 클래스 넣기
+************************************************/
+// (1) 이벤트 대상 : .btn-ham
+const $btnHam = $(".btn-ham");
+// (2) 변경 대상 : #top-area 
+const $topArea = $("#top-area");
+
+// (3) 이벤트 대상 클릭시 변경 대상에 클래스 토글로 on 넣기
+$btnHam.on("click", () => {
+  $topArea.toggleClass("on");
+}); // click //////////
