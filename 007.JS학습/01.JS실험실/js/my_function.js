8c1418f5aeeb07198b8261942a346f82469a0a76
KGH <ehsrhdiddl@naver.com>
2025-10-02 15:58:55 +0900

    +

--- 007.JS학습/01.JS실험실/js/my_function.js
+++ 007.JS학습/01.JS실험실/js/my_function.js
@@ -7,6 +7,10 @@ const myFn = {
   qsaEl: (el, x) => el.querySelectorAll(x),
   // 이벤트 셋팅함수
   addEvt: (el, evt, fn) => el.addEventListener(evt, fn),
+  // 바운딩 함수 : top값
+  getBCR: (x) => x.getBoundingClientRect().top,
+  // 바운딩 함수 : left값
+  getBCR2: (x) => x.getBoundingClientRect().left,
 }; ///// myFn 객체 ///////
 
 // import해서 사용할 파일이므로 객체를 내보내야함!
