fb2849a2913937bb8bbc03cb52be54268fc001a5
KGH <ehsrhdiddl@naver.com>
2025-10-15 14:56:23 +0900

    Update unlimit_jquery.js

--- 004.HTML학습/06.ShoppingMall_PJ/js/unlimit_jquery.js
+++ 004.HTML학습/06.ShoppingMall_PJ/js/unlimit_jquery.js
@@ -77,6 +77,9 @@ const $indic = $slideBox.find(".indic li");
 // 오른쪽 버튼 클릭시
 // 원리: translate X축 이동값을 -100%로 변경
 $abtn.click(function () {
+  // 광클금지함수 호출 셋팅하기
+  if (blockClick()) return;
+
   // 버튼구분하기 : 오른쪽버튼(.ab2)이면 true
   let isR = $(this).is(".ab2");
   console.log("오른쪽?", isR);
@@ -87,16 +90,16 @@ $abtn.click(function () {
   if (isR) {
     $slide.animate(
       { translate: "-100%" }, // CSS변경
-      400, // 시간
+      TIME_GAP, // 시간
       () => {
         // 애니후 실행함수 시작
         $slide // 주인공은? 슬라이드!
           // (1) 맨앞요소 맨뒤로 이동
           .append($slide.find("li").first())
           // (2) 이때 translate값 초기화
-          .css({ translate: "0" });
+          .css({ translate: "0%" });
       } /// 애니후 실행함수 끝 ///
-    );
+    ); /// animate //////
   } //// if //////
 
   // 2. 왼쪽 버튼일때 //////
@@ -105,8 +108,24 @@ $abtn.click(function () {
       // (1) 슬라이드가 먼저 맨뒤li를 맨앞으로 이동
       .prepend($slide.find("li").last())
       // (2) 이때 translate값 초기화
-      .css({ translate: "-100%" });
+      .css({ translate: "-100%" })
+      // (3) 오른쪽 방향으로 슬라이드 이동애니
+      .animate({ translate: "0%" }, TIME_GAP);
+    // -> 주의! 0일 경우에도 단위를 반드시 써야
+    // 애니메이션이 잘 적용된다!
   } //// else //////
+
+  // 3. 슬라이드 위치표시 블릿
+  // (1) 블릿 대상: .indic li
+  // 해당 블릿은 오른쪽 버튼일때 순번 1, 왼쪽버튼일때 순번 0 슬라이드의 data-seq값을 읽어오면 된다!
+  let currIdx = $slide
+    .find("li")
+    .eq(isR ? 1 : 0)
+    .attr("data-seq");
+  console.log("읽은순번:", currIdx);
+
+  // (2) 블릿 li클래스 on넣기(나머지는 빼기)
+  $indic.eq(currIdx).addClass("on").siblings().removeClass("on");
 }); ////// click 메서드 //////////
 
 /******************************** 
@@ -146,19 +165,39 @@ const IV_TIME = 2000;
 const TO_TIME = 5000;
 
 // 자동호출함수 최초호출 ////
-// slideAuto();
+slideAuto();
 
 // [3] 자동호출함수 ////////////////
 function slideAuto() {
   // 지우기위해 전역변수 autoI에 할당함
   autoI = setInterval(() => {
-    // 맨앞div 맨뒤로 이동
-    $target.append($target.find("div").first());
+    // 1. 오른쪽버튼 클릭시 작동과 동일!
+    $slide.animate(
+      { translate: "-100%" }, // CSS변경
+      TIME_GAP, // 시간
+      () => {
+        // 애니후 실행함수 시작
+        $slide // 주인공은? 슬라이드!
+          // (1) 맨앞요소 맨뒤로 이동
+          .append($slide.find("li").first())
+          // (2) 이때 translate값 초기화
+          .css({ translate: "0%" });
+      } /// 애니후 실행함수 끝 ///
+    ); /// animate //////
+
+    // 2. 슬라이드 위치표시 블릿
+    // (1) 블릿 대상: .indic li
+    // 해당 블릿은 오른쪽 버튼일때 순번 1 슬라이드의 data-seq값을 읽어오면 된다!
+    let currIdx = $slide.find("li").eq(1).attr("data-seq");
+    // console.log("읽은순번:", currIdx);
+
+    // (2) 블릿 li클래스 on넣기(나머지는 빼기)
+    $indic.eq(currIdx).addClass("on").siblings().removeClass("on");
   }, IV_TIME);
 } ////// slideAuto 함수 //////
 
 // 이동버튼 클릭시 지우기함수 호출하기 ////
-// $('.abtn').click(clearAuto);
+$(".abtn").click(clearAuto);
 
 // [4] 지우기 함수 /////////////////
 function clearAuto() {
