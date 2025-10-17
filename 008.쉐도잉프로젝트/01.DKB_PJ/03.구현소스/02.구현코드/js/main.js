7fe1fe4c2830c77ea87605ae9e538c25d58fb69d
KGH <ehsrhdiddl@naver.com>
2025-10-17 13:18:25 +0900

    +

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
@@ -1,44 +1,14 @@
 // 도깨비 PJ 메인 페이지 JS - main.js /////////
 
-///////////////////////////////////////////////////
-// 1. 큐브로고박스 일정간격으로 클래스 넣었다 빼기 ///
-//////////////////////////////////////////////////
-
-// -> 로고가 일정 간격으로 회전함!
-// 지금은 1.5초씩 alternate하였으므로 총 3초걸림
-
-// 1-1. 대상선정 : .cube-logo
-const cubeLogo = document.querySelector(".cube-logo");
-
-// 1-2. 클래스 셋팅함수 ////////////////
-const setClass = () => {
-  // 클래스넣기
-  cubeLogo.classList.add("rotate-cube");
-
-  // 3초후 클래스 제거하기
-  // setTimeout(함수, 시간);
-  setTimeout(() => {
-    cubeLogo.classList.remove("rotate-cube");
-  }, 3000);
-}; /////////// setClass 함수 //////////
-
-// 1-3. 일정간격으로 클래스 셋팅함수 호출하기 ///////
-// setInterval(함수, 시간);
-// 일정시간 간격으로 함수가 실행됨!
-setInterval(setClass, 10000);
-/// 10초간격으로 실행됨! //////
-
-// 1-4. 처음에 회전하도록 클래스 셋팅함수 호출하기
-setClass();
-
 /************************************************ 
-    최신 동영상 파트에 스와이퍼 적용하기
+    2. 최신 동영상 파트에 스와이퍼 적용하기
 ************************************************/
 const videoSwiper = new Swiper(".clip-box", {
-  // 자동 플레이 설정
+  // 자동플레이설정
   autoplay: {
     delay: 3000, // 지연시간
-    disableOnInteraction: false, // 건드리면 멈췄다가 다시 재개함(false)
+    disableOnInteraction: false,
+    // 건드리면 멈췄다가 다시 재개함!(false)
   },
 
   // 화면크기별 스와이퍼 슬라이드 개수
@@ -68,7 +38,7 @@ const videoSwiper = new Swiper(".clip-box", {
 });
 
 // 버튼 요소 선택 ///
-// -> 선택시 주의: .ab1, .ab2는 배너에도 있음
+// -> 선택시 주의: .ab1,.ab2는 배너에도 있음!
 const btnPrev = document.querySelector("#video-part .ab1");
 const btnNext = document.querySelector("#video-part .ab2");
 // console.log(btnPrev, btnNext);
@@ -78,19 +48,19 @@ btnPrev.style.display = "none";
 
 // 다음버튼 클릭시 Swiper API를 이용한 코딩하기!!!
 btnNext.addEventListener("click", () => {
+  // console.log("다음버튼 클릭");
   videoSwiper.slideNext();
 });
 
 // 이전버튼 클릭시 Swiper API를 이용한 코딩하기!!!
 btnPrev.addEventListener("click", () => {
-  // console.log("다음버튼 클릭");
   videoSwiper.slidePrev();
 });
 
 // 스와이퍼 슬라이드가 변경될때 발생 이벤트는? slideChange
 videoSwiper.on("slideChange", () => {
-  // console.log("맨처음인가?", videoSwiper.isBeginning);
-  // console.log("맨끝인가?", videoSwiper.isEnd);
+  //   console.log("맨처음인가?", videoSwiper.isBeginning);
+  //   console.log("맨끝인가?", videoSwiper.isEnd);
 
   // 맨처음인가? 맨끝인가?에 따른 분기 //////
   // 1. 맨처음엔 => 이전버튼 비활성화
@@ -109,16 +79,3 @@ videoSwiper.on("slideChange", () => {
     btnPrev.style.display = "block";
   } /// else ////
 }); ///// slideChange //////
-
-/************************************************ 
-    햄버거 버튼 클릭시 상단영역에 클래스 넣기
-************************************************/
-// (1) 이벤트 대상 : .btn-ham
-const $btnHam = $(".btn-ham");
-// (2) 변경 대상 : #top-area 
-const $topArea = $("#top-area");
-
-// (3) 이벤트 대상 클릭시 변경 대상에 클래스 토글로 on 넣기
-$btnHam.on("click", () => {
-  $topArea.toggleClass("on");
-}); // click //////////
