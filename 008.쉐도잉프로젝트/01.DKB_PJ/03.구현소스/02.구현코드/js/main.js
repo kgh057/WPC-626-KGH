b6ffe687e617f0dc5ba50ff749a3a19b1c6e62e2
KGH <ehsrhdiddl@naver.com>
2025-10-23 09:41:14 +0900

    +

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
@@ -1,14 +1,8 @@
 // 도깨비 PJ 메인 페이지 JS - main.js /////////
 
 // 도깨비 데이터 불러오기 ////
-import {
-  previewData,
-  catData,
-  liveData,
-  posterData,
-  clipData,
-} from "../data/dkb_data.js";
-console.log(clipData);
+import { previewData, catData, liveData, posterData, clipData } from "../data/dkb_data.js";
+// console.log(clipData);
 
 // 스크롤액션 불러오기 /////
 import scrollAction from "./scroll_action.js";
@@ -22,8 +16,9 @@ scrollAction();
 // 데이터를 요소에 넣어서 화면에 출력하는 것을
 // 데이터 바인딩 이라고 한다!
 // (1) 바인딩 대상 : .preview-box ul.cont-box
-const previewArea = document.querySelector(".preview-area ul.cont-box");
-console.log(previewArea);
+const previewArea = 
+document.querySelector('.preview-area ul.cont-box');
+// console.log(previewArea);
 // (2) 바인딩 데이터 : previewData
 // -> 요구사항 : 기존 배열데이터를 내림차순 정리하여
 // 8개의 데이터만 다시 변수에 할당한다!
@@ -38,11 +33,11 @@ previewData.sort((a, b) => b.idx - a.idx);
 
 // 새로운 배열에 앞쪽 8개의 데이터 수집하기
 let newPreviewData = [];
-for (let i = 0; i < 8; i++) {
+for(let i=0; i<8; i++){
   newPreviewData.push(previewData[i]);
   // push(값) -> 배열에 값추가
 }
-console.log("새로운 미리보기 배열", newPreviewData);
+// console.log('새로운 미리보기 배열',newPreviewData);
 
 /* (3) 반복 구조
 <li>
@@ -55,23 +50,21 @@ console.log("새로운 미리보기 배열", newPreviewData);
 // -> 새로운 배열값 만들기는 맵조잉~!!!(map().join(''))
 // -> 여기서는 내림차순 상위 8개 데이터만 돌아준다!
 // -> newPreviewData 사용!
-previewArea.innerHTML = newPreviewData
-  .map(
-    (v) => `
+previewArea.innerHTML = 
+newPreviewData.map(v=>`
   <li>
     <h3>${v.title}</h3>
     <p>${v.story}</p>
   </li>
-  `
-  )
-  .join("");
+  `).join('');
 
 /************************************************ 
     2. 캐릭터 소개 영역 데이터 바인딩하기
 ************************************************/
 // 바인딩 대상 : .cat-box
-const catBox = document.querySelector(".cat-box");
-console.log(catBox);
+const catBox = 
+document.querySelector('.cat-box');
+// console.log(catBox);
 
 // 데이터 바인딩 태그구조
 /* 
@@ -96,9 +89,8 @@ console.log(catBox);
 */
 
 // 데이터 바인딩하기 -> 맵죠잉~~!
-catBox.innerHTML = catData
-  .map(
-    (v) => `
+catBox.innerHTML = 
+catData.map(v=>`
   <!-- ${v.actorName}박스 -->
 <div>
   <!-- 이미지박스 -->
@@ -106,7 +98,7 @@ catBox.innerHTML = catData
     <img src="./images/dc${v.idx}.png" alt="${v.actorName}" />
     <figcaption>
       <img src="./images/d0${v.idx}.png" alt="${v.actorName}진한글자" />
-      <img src="./images/d0${v.idx + v.idx}.png" alt="도깨비흐린글자" />
+      <img src="./images/d0${v.idx+v.idx}.png" alt="도깨비흐린글자" />
     </figcaption>
   </figure>
   <!-- 글박스 -->
@@ -114,32 +106,31 @@ catBox.innerHTML = catData
     <!-- 타이틀 -->
     <h3>${v.catTitle}</h3>
     <!-- 내용 -->
-    <p>${v.catContent.replace(/\n/g, "<br />")}</p>
+    <p>${v.catContent.replace(/\n/g,'<br />')}</p>
   </aside>
 </div>  
-  `
-  )
-  .join("");
+  `).join('');
 
-// [ 기존 캐릭터 소개글에서 엔터부분에 br태그 넣기 ]
-// 문자열중 특정문자를 변경해주는 메서드는? replace()
-// 문자열.replace(바꿀문자열,바뀔문자열)
-// -> 이렇게 하면 문자 하나만 변경되므로
-// 정규식을 사용해서 선택해야 모두 변경된다!
-// 정규식은 문자열의 패턴을 찾아주는 문법!
-// -> .replace(/선택문자열/g,'바뀔문자열')
-// -> 정규식은 슬래쉬 사이에 씀
-// -> 정규식 슬래쉬 뒤에 g문자는 global(전체)라는 의미
-// -> 그러면 모두 찾아서 변경함
-// -> \n은 엔터기호를 찾아냄
-// .replace(/\n/g,'<br/>')
+  // [ 기존 캐릭터 소개글에서 엔터부분에 br태그 넣기 ]
+  // 문자열중 특정문자를 변경해주는 메서드는? replace()
+  // 문자열.replace(바꿀문자열,바뀔문자열)
+  // -> 이렇게 하면 문자 하나만 변경되므로
+  // 정규식을 사용해서 선택해야 모두 변경된다!
+  // 정규식은 문자열의 패턴을 찾아주는 문법!
+  // -> .replace(/선택문자열/g,'바뀔문자열')
+  // -> 정규식은 슬래쉬 사이에 씀
+  // -> 정규식 슬래쉬 뒤에 g문자는 global(전체)라는 의미
+  // -> 그러면 모두 찾아서 변경함
+  // -> \n은 엔터기호를 찾아냄
+  // .replace(/\n/g,'<br/>')
 
 /************************************************ 
     3. 현장포토 영역 데이터 바인딩하기
 ************************************************/
 // (1) 바인딩 대상 : .live-box
-const liveBox = document.querySelector(".live-box");
-console.log(liveBox);
+const liveBox = 
+document.querySelector('.live-box');
+// console.log(liveBox);
 
 // (2) 바인딩 데이터 : liveData
 // (3) 바인딩 태그구조
@@ -158,11 +149,9 @@ console.log(liveBox);
 */
 
 // (4) 바인딩하기 -> 맵죠잉~~!
-liveBox.innerHTML =
-  "<ul>" +
-  liveData
-    .map(
-      (v) => `
+liveBox.innerHTML = 
+'<ul>'+
+liveData.map(v=>`
   <li data-idx="${v.idx}">
     <figure>
       <img
@@ -172,17 +161,16 @@ liveBox.innerHTML =
       <figcaption>${v.title}</figcaption>
     </figure>
   </li>
-  `
-    )
-    .join("") +
-  "</ul>";
+  `).join('')
++'</ul>';
 
 /************************************************ 
     4. 대표포스터 영역 데이터 바인딩하기
 ************************************************/
 // (1) 바인딩 대상 : .poster-box
-const posterBox = document.querySelector(".poster-box");
-console.log(posterBox);
+const posterBox = 
+document.querySelector('.poster-box');
+// console.log(posterBox);
 
 // (2) 바인딩 데이터 : posterData
 // (3) 바인딩 태그구조
@@ -201,11 +189,9 @@ console.log(posterBox);
 */
 
 // (4) 바인딩하기 -> 맵죠잉~~!
-posterBox.innerHTML =
-  "<ul>" +
-  posterData
-    .map(
-      (v) => `
+posterBox.innerHTML = 
+'<ul>'+
+posterData.map(v=>`
   <li data-idx="${v.idx}">
     <figure>
       <img
@@ -215,17 +201,16 @@ posterBox.innerHTML =
       <figcaption>${v.title}</figcaption>
     </figure>
   </li>
-  `
-    )
-    .join("") +
-  "</ul>";
+  `).join('')
++'</ul>';
 
 /************************************************ 
     5. 대표포스터 영역 데이터 바인딩하기
 ************************************************/
 // (1) 바인딩 대상 : .clip-box
-const clipBox = document.querySelector(".clip-box");
-console.log(clipBox);
+const clipBox = 
+document.querySelector('.clip-box');
+// console.log(clipBox);
 
 // (2) 바인딩 데이터 : clipData
 // (3) 바인딩 태그구조
@@ -249,11 +234,9 @@ console.log(clipBox);
 */
 
 // (4) 바인딩하기 -> 맵죠잉~~!
-clipBox.innerHTML =
-  '<ul class="slide swiper-wrapper">' +
-  clipData
-    .map(
-      (v) => `
+clipBox.innerHTML = 
+'<ul class="slide swiper-wrapper">'+
+clipData.map(v=>`
   <li class="swiper-slide" data-idx="${v.idx}">
     <div class="clip-mv-box">
       <img
@@ -264,10 +247,8 @@ clipBox.innerHTML =
     <h4>${v.subtit}</h4>
     <h3>${v.title}</h3>
   </li>
-  `
-    )
-    .join("") +
-  "</ul>";
+  `).join('')
++'</ul>';
 
 /************************************************ 
     6. 최신 동영상 파트에 스와이퍼 적용하기
@@ -310,14 +291,14 @@ const videoSwiper = new Swiper(".clip-box", {
 // -> 선택시 주의: .ab1,.ab2는 배너에도 있음!
 const btnPrev = document.querySelector("#video-part .ab1");
 const btnNext = document.querySelector("#video-part .ab2");
-// console.log(btnPrev, btnNext);
+// // console.log(btnPrev, btnNext);
 
 // 이전버튼은 처음 로딩시 숨기기
 btnPrev.style.display = "none";
 
 // 다음버튼 클릭시 Swiper API를 이용한 코딩하기!!!
 btnNext.addEventListener("click", () => {
-  // console.log("다음버튼 클릭");
+  // // console.log("다음버튼 클릭");
   videoSwiper.slideNext();
 });
 
@@ -328,8 +309,8 @@ btnPrev.addEventListener("click", () => {
 
 // 스와이퍼 슬라이드가 변경될때 발생 이벤트는? slideChange
 videoSwiper.on("slideChange", () => {
-  //   console.log("맨처음인가?", videoSwiper.isBeginning);
-  //   console.log("맨끝인가?", videoSwiper.isEnd);
+  //   // console.log("맨처음인가?", videoSwiper.isBeginning);
+  //   // console.log("맨끝인가?", videoSwiper.isEnd);
 
   // 맨처음인가? 맨끝인가?에 따른 분기 //////
   // 1. 맨처음엔 => 이전버튼 비활성화
